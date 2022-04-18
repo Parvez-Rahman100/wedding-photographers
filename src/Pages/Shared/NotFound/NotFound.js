d93c98d9bbd847ab51e5704e968da794c648c22f
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-04-18 23:05:40 +0600

    404 updated

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>The page you are looking for not found</h1>
-            <h1>404</h1>
+            <h1 className='text-center my-3'>The page you are looking for not found</h1>
+            <h1 className='text-center my-3'>404</h1>
         </div>
     );
 };
