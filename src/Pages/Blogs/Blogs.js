7c8844ae9b763aae80da03310d6d228bb21067d5
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-04-18 21:35:33 +0600

    blogs update

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -5,7 +5,7 @@ const Blogs = () => {
         <div className='container'>
             <div >
                 <h1 className='text-center text-success my-3'>Difference between authentication and authorization</h1>
-                <div className='d-lg-flex flex-sm-row justify-content-between bg-success'>
+                <div style={{backgroundColor : 'rgba(128, 142, 155,1.0)'}} className='d-lg-flex flex-sm-row justify-content-between'>
                 <div className='border border-2 px-3 rounded'>
                     <h2>Authentication</h2>
                     <p>Authorization means who is the user.</p>
@@ -24,11 +24,11 @@ const Blogs = () => {
             </div>
             <div className=' p-4 rounded'>
                 <h3 className='text-center text-success  my-3'>Why am I using firebase? What other options do I have to implement authentication?</h3>
-                <p className='bg-success p-4'>Firebase have so many features.We have firebase authentication.We can authenticate an user using email,password, google , github , facebook etc. Then we have database .We can get real time database so we can see changes in real time.We have storage features here.In firebase we have another feature that is so important . That is hosting . Hosting is so easy here. And there is a lot of features.</p>
+                <p style={{backgroundColor : 'rgba(128, 142, 155,1.0)'}} className='p-4'>Firebase auth provides secure and reliable authentication. We can implemet other authentication like fire-base hooks.It is so much easier to use and reliable.We can login , signup and signup by firebase authentication easily by changing state.</p>
             </div>
             <div className=' p-4 rounded'>
                 <h3 className='text-center text-success  my-3'>What other services does firebase provide other than authentication?</h3>
-                <p className='bg-success p-4'>Firebase have so many features.We have firebase authentication.We can authenticate an user using email,password, google , github , facebook etc. Then we have database .We can get real time database so we can see changes in real time.We have storage features here.In firebase we have another feature that is so important . That is hosting . Hosting is so easy here. And there is a lot of features.</p>
+                <p style={{backgroundColor : 'rgba(128, 142, 155,1.0)'}} className='p-4'>Firebase have so many features.We have firebase authentication.We can authenticate an user using email,password, google , github , facebook etc. Then we have database .We can get real time database so we can see changes in real time.We have storage features here.In firebase we have another feature that is so important . That is hosting . Hosting is so easy here. And there is a lot of features.</p>
             </div>
         </div>
     );
